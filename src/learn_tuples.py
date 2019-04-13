4b26b8836cc73ec65bae8e8b427ec609ac277eac
vishnusurya11 <vishnusurya11@google.com>
2019-04-13 16:48:01 -0700

    added tuple methods

--- src/learn_tuples.py
+++ src/learn_tuples.py
@@ -4,7 +4,13 @@
 
 a = (1,2,3)
 
-print(a)
+print(f'the tuple a :{a}')
 
+#to get type 
 
+print(f'the type of a :{type(a)}')
 
+print(f'length of the tuple a : {len(a)}')
+
+print(f'index of 3 in a is : {a.index(3)}')
+print(f'count of 3 in a is : {a.count(3)}')
